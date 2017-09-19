0c8c2de000ec48d41cbc9b498c55b28cdf7b459c
Yaneeve <yaneeve.shekel@gmail.com>
2017-09-19 21:10:55 +0300

    first commit of the day, I wish to refactor

--- src/main/scala/yaneeve/elevator/single/updown/BasicElevatorAlgorithm.scala
+++ src/main/scala/yaneeve/elevator/single/updown/BasicElevatorAlgorithm.scala
@@ -1,9 +1,9 @@
 package yaneeve.elevator
 package single.updown
 
-import math.{min, abs}
-
 import com.softwaremill.quicklens._
+
+import scala.collection.SortedSet
 /**
   * Created by yaneeve on 9/18/17.
   */
@@ -11,18 +11,24 @@ class BasicElevatorAlgorithm extends DispatchAlgorithm {
   override def dispatch(dispatcher: Dispatcher, pickupRequest: PickupRequest): Dispatcher = {
 //    val enqueued = dispatcher.copy(requestQueue = dispatcher.requestQueue.enqueue(pickupRequest))
 
-    val receivedDispatcher = ascertainDirection(dispatcher.elevator.floor, pickupRequest.initialPickupRequest.floor) match {
-      case Up => dispatcher.copy(upRequests = dispatcher.upRequests + pickupRequest)
-      case Down => dispatcher.copy(downRequests  = dispatcher.downRequests + pickupRequest)
+    /*val receivedDispatcher = */ascertainDirection(dispatcher.elevator.floor, pickupRequest.initialPickupRequest.floor) match {
+      case Up =>
+        val modifiedUpRequests: SortedSet[PickupRequest] = dispatcher.upRequests + pickupRequest
+        dispatcher.copy(upRequests = modifiedUpRequests).modify(_.elevator.destination).
+          setTo(modifiedUpRequests.head.destination)
+      case Down =>
+        val modifiedDownRequests: SortedSet[PickupRequest] = dispatcher.downRequests + pickupRequest
+        dispatcher.copy(downRequests  = modifiedDownRequests).modify(_.elevator.destination).
+          setTo(modifiedDownRequests.head.destination)
       case Idle => // This is a "don't close the door" type of request
         dispatcher
     }
-    receivedDispatcher.elevator.direction match {
-      case Up => receivedDispatcher.elevator.copy(destination = receivedDispatcher.upRequests.head.destination)
-      case Down => receivedDispatcher.elevator.copy(destination = receivedDispatcher.downRequests.head.destination)
-      case Idle => ??? // choose closest request
-    }
-    ???
+//    receivedDispatcher.elevator.direction match {
+//      case Up => receivedDispatcher.modify(_.elevator.destination = receivedDispatcher.upRequests.head.destination)
+//      case Down => receivedDispatcher.elevator.copy(destination = receivedDispatcher.downRequests.head.destination)
+//      case Idle => ??? // choose closest request
+//    }
+//    ???
   }
 
   override def step(dispatcher: Dispatcher): Dispatcher = {
