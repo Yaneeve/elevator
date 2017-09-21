5fee3582a59e42b0e332a887ad45e33364f25c46
Yaneeve <yaneeve.shekel@gmail.com>
2017-09-21 23:04:04 +0300

    single test seems to work

--- src/test/scala/yaneeve/elevator/single/updown/BasicElevatorTest.scala
+++ src/test/scala/yaneeve/elevator/single/updown/BasicElevatorTest.scala
@@ -45,57 +45,51 @@ object BasicElevatorTest extends TestSuite {
       )
       bookedRequests ==> Elevator(currentFloor = GroundFloor + 1, direction = Up,
         inTravelDirectionRequests = Set(HighestFloor, 32), outOfTravelDirectionRequests = Set(16))
-//      val elevatorDispatchedToHighestFloor = elevator.copy(travelUpRequests = Set(HighestFloor))
-//      alg.receiveFloorRequest(elevator, HighestFloor) ==> elevatorDispatchedToHighestFloor
-//      val elevatorGearingUpToMove = elevatorDispatchedToHighestFloor.copy(direction = Up)
-//      alg.step(elevatorDispatchedToHighestFloor) ==> elevatorGearingUpToMove
-//      val elevatorHasMovedUp = elevatorGearingUpToMove.copy(currentFloor = GroundFloor + 1)
-//      alg.step(elevatorGearingUpToMove) ==> elevatorHasMovedUp
-//      val movedUpTo32 = (1 until 32).foldLeft(elevatorHasMovedUp){ case (elev, _) => alg.step(elev)}
-//      movedUpTo32 ==> elevatorHasMovedUp.copy(currentFloor = 32)
-//      alg.receiveFloorRequest(movedUpTo32, 32) ==> movedUpTo32
-//      val withTravelDownReq = movedUpTo32.copy(travelDownRequests = Set(30))
-//      alg.receiveFloorRequest(movedUpTo32, 30) ==> withTravelDownReq
-//      val aStopOn34Scheduled = withTravelDownReq.copy(travelUpRequests = withTravelDownReq.travelUpRequests + 34)
-//      alg.receiveFloorRequest(withTravelDownReq, 34) ==> aStopOn34Scheduled
-//      val movedTo33 = aStopOn34Scheduled.copy(currentFloor = 33)
-//      alg.step(aStopOn34Scheduled) ==> movedTo33
-//      val movedTo34 = movedTo33.copy(currentFloor = 34, travelUpRequests = movedTo33.travelUpRequests.filterNot(_ == 34))
-//      alg.step(movedTo33) ==> movedTo34
-//      alg.step(movedTo34) ==> movedTo34.copy(currentFloor = 35)
+      val nextMove = alg.step(bookedRequests)
+      nextMove ==> Elevator(currentFloor = GroundFloor + 2, direction = Up,
+        inTravelDirectionRequests = Set(HighestFloor, 32), outOfTravelDirectionRequests = Set(16))
+      val fastForward = (1 to 30).foldLeft(nextMove){ case (elev, _) => alg.step(elev) }
+      fastForward ==> Elevator(currentFloor = GroundFloor + 32, direction = Up,
+        inTravelDirectionRequests = Set(HighestFloor), outOfTravelDirectionRequests = Set(16))
+      val floorRequests = alg.receiveFloorRequest(
+        alg.receiveFloorRequest(fastForward, 72), 23)
+      val remaining = HighestFloor - (GroundFloor + 32)
+      val upToTheTopAndBeyond = (0 to remaining).foldLeft(floorRequests){ case (elev, _) => alg.step(elev) }
+      upToTheTopAndBeyond ==> Elevator(currentFloor = HighestFloor - 1, direction = Down,
+        inTravelDirectionRequests = Set(23, 16))
     }
-//
-//    'second - {
-//      val elevator = Elevator(12, Down, travelUpRequests = Set(15,17), travelDownRequests = Set(9))
-//      val alg = new BasicElevatorAlgorithm
-//      val checkpoint1 = alg.step(
-//        alg.step(
-//          alg.step(elevator)
-//        ))
-//      checkpoint1 ==> Elevator(9, Idle, travelUpRequests = Set(15, 17), travelDownRequests = Set.empty)
-//      val checkpoint2 = alg.step(checkpoint1)
-//      checkpoint2 ==> Elevator(9, Up, travelUpRequests = Set(15, 17), travelDownRequests = Set.empty)
-//      val checkpoint3 = alg.step(checkpoint2)
-//      checkpoint3 ==> Elevator(10, Up, travelUpRequests = Set(15, 17), travelDownRequests = Set.empty)
-//      // so we employed foldLeft in the above test, but would it not have been nice to have a for-comprehension?
-//      // TODO, see if we can trandform alg somehow into a Monad with `map` and `flatMap` methods
-//      val checkpoint4 = alg.step(
-//        alg.step(
-//          alg.step(
-//            alg.step(
-//              alg.step(
-//                alg.step(
-//                  alg.step(
-//                    alg.step(checkpoint3)
-//                  )
-//                )
-//              )
-//            )
-//          )
-//        )
-//      )
-//      checkpoint4 ==> Elevator(17, Idle)
-//
-//    }
+
+    'second - {
+      val elevator = Elevator(12, Down, outOfTravelDirectionRequests = Set(15,17), inTravelDirectionRequests = Set(9))
+      val alg = new BasicElevatorAlgorithm
+      val checkpoint0 = alg.step(
+        alg.step(elevator))
+      checkpoint0 ==> Elevator(10, Down, inTravelDirectionRequests = Set(9), outOfTravelDirectionRequests = Set(15, 17))
+      val checkpoint1 = alg.step(checkpoint0)
+      checkpoint1 ==> Elevator(9, Up, inTravelDirectionRequests = Set(15, 17), outOfTravelDirectionRequests = Set.empty)
+      val checkpoint2 = alg.step(checkpoint1)
+      checkpoint2 ==> Elevator(10, Up, inTravelDirectionRequests = Set(15, 17), outOfTravelDirectionRequests = Set.empty)
+      val checkpoint3 = alg.step(checkpoint2)
+      checkpoint3 ==> Elevator(11, Up, inTravelDirectionRequests = Set(15, 17), outOfTravelDirectionRequests = Set.empty)
+      // so we employed foldLeft in the above test, but would it not have been nice to have a for-comprehension?
+      // TODO, see if we can trandform alg somehow into a Monad with `map` and `flatMap` methods
+      val checkpoint4 = alg.step(
+        alg.step(
+          alg.step(
+            alg.step(
+              alg.step(
+                alg.step(
+                  alg.step(
+                    alg.step(checkpoint3)
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+      checkpoint4 ==> Elevator(17, Idle)
+
+    }
   }
 }
